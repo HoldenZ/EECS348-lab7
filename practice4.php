6192094c8290b839dcde964093a9803e27d40ab6
HoldenZ <123589710+HoldenZ@users.noreply.github.com>
2023-04-09 14:59:09 -0500

    Add files via upload

---
+++ practice4.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Multiplication Table</title>
+</head>
+<body>
+<?php
+if(isset($_POST['size']))
+{
+    $number = $_POST['size'];
+    echo "<h2>Multiplication Table of $number</h2>";
+    echo "<table border='1'>";
+    echo "<tr><th>&nbsp;</th>";
+
+    // Create column headers
+    for($i = 1; $i <= $number; $i++)
+    {
+        echo "<th>$i</th>";
+    }
+
+    echo "</tr>";
+
+    // Create rows and cells
+    for($i = 1; $i <= $number; $i++)
+    {
+        echo "<tr><th>$i</th>";
+        for($j = 1; $j <= $number; $j++)
+        {
+            $product = $i * $j;
+            echo "<td>$product</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+?>
+</body>
+</html>
